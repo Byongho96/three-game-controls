036062d6153ba9fb2a47011d66de0af546b0071e
Byongho <unlike96@gmail.com>
2025-01-08 19:42:52 +0900

    fix: change class name returned by mixin

--- src/mixins/KeyboardMixin.ts
+++ src/mixins/KeyboardMixin.ts
@@ -6,7 +6,7 @@ export interface HasDragMethod {
 
 function KeyboardMixin<T extends Constructor<FirstPersonControls>>( Base: T ): Constructor<HasDragMethod> & T {
 
-	return class DragMixin extends Base {
+	return class KeyboardMixin extends Base {
 
 		keyToActions: Record<string, Action[]> = {
 			'KeyW': [ 'MOVE_FORWARD' ],
