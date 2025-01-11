e9f5f57e7ff0283d7f639e14d33ed4779428dc75
Byongho96 <Byongho96@users.noreply.github.com>
2025-01-11 15:25:53 +0000

    deploy: 1d6b84bc3e167deda900046fb66b50542bcab1a8

--- dist/controls/FPKeyboardDragControls.d.ts
+++
@@ -1,5 +0,0 @@
-import { FirstPersonControls } from './base/FirstPersonControls';
-declare const FPKeyboardDragControls_base: Constructor<import("../mixins/KeyboardMixin").HasKeyboardMethod> & Constructor<import("../mixins/DragMixin").HasDragMethod> & typeof FirstPersonControls;
-declare class FPKeyboardDragControls extends FPKeyboardDragControls_base {
-}
-export { FPKeyboardDragControls };
--- dist/mixins/DragMixin.d.ts
+++
@@ -1,9 +0,0 @@
-import { type FirstPersonControls, type Action } from '../controls/base/FirstPersonControls';
-export interface HasDragMethod {
-    dragXActions: Action[];
-    dragYActions: Action[];
-    dragDampingFactor: number;
-    enableDragDamping: boolean;
-}
-declare function DragMixin<T extends Constructor<FirstPersonControls>>(Base: T): Constructor<HasDragMethod> & T;
-export { DragMixin };
