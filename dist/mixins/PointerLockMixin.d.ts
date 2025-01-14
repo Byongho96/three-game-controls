ecce525f025b6954d701bfe6962fdfe8a8ac82c4
Byongho96 <Byongho96@users.noreply.github.com>
2025-01-14 04:03:45 +0000

    deploy: 771be3abc9b2f4149c30280273bc17a860ca648b

--- dist/mixins/PointerLockMixin.d.ts
+++
@@ -1,9 +0,0 @@
-import { type FirstPersonControls, type Action } from '../controls/core/FirstPersonControls';
-export interface HasPointerLockMethod {
-    pointerLockXActions: Action[];
-    pointerLockYActions: Action[];
-    pointerLockDampingFactor: number;
-    enablePointerLockDamping: boolean;
-}
-declare function PointerLockMixin<T extends Constructor<FirstPersonControls>>(Base: T): Constructor<HasPointerLockMethod> & T;
-export { PointerLockMixin };
