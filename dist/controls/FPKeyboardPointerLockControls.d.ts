3d47af6650bb388a0123b746b291ab39796864c0
Byongho96 <Byongho96@users.noreply.github.com>
2025-02-04 01:49:44 +0000

    deploy: c14d29040306811112468443229a296ec1163ab5

--- dist/controls/FPKeyboardPointerLockControls.d.ts
+++
@@ -1,5 +0,0 @@
-import { FirstPersonControls } from './core/FirstPersonControls';
-declare const FPKeyboardPointerLockControls_base: Constructor<import("../mixins/KeyboardMixin").HasKeyboardMethod> & Constructor<import("../mixins/PointerLockMixin").HasPointerLockMethod> & typeof FirstPersonControls;
-declare class FPKeyboardPointerLockControls extends FPKeyboardPointerLockControls_base {
-}
-export { FPKeyboardPointerLockControls };
